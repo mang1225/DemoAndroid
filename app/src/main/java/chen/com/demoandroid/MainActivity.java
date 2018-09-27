a017b53c19d5d268b5924dbe648eba821a18385f
okman999@aliyun.com <123456z>
2018-09-27 09:39:29 +0800

    update

--- app/src/main/java/chen/com/demoandroid/MainActivity.java
+++ app/src/main/java/chen/com/demoandroid/MainActivity.java
@@ -1,10 +1,7 @@
 package chen.com.demoandroid;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import chen.com.demoandroid.view.autoscrollviewpager.ViewPagerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    startActivity(new Intent(this, EndlessGridLayoutActivity.class));
   }
 }
